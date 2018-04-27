e2825346afe92d57a2e05b3af17fc74fa11a6e69
Овчаренко Виталий <ovcharenko@SOFTCOM.local>
2018-04-27 09:52:52 +0300

    27

--- hostel/js/script.js
+++ hostel/js/script.js
@@ -1,91 +1,105 @@
-$(function(){
-        
+$(function () {
+
     $(window).on("load", function () {
-                $(window).scroll(function () {
-                    $(this).scrollTop() > $(this).height() ? $(".top_button").addClass("active") : $(".top_button").removeClass("active");
-                }); 
-                $(".top_button").click(function () {
-                    $("html, body").stop().animate({scrollTop: 0}, "slow", "swing");
-            });
-
-         });
-
-        $('.togglemenu').click(function(){
-            $(this).toggleClass('active'); 
-            $('.top_menu ul').slideToggle(400);
+        $(window).scroll(function () {
+            $(this).scrollTop() > $(this).height() ? $(".top_button").addClass("active") : $(".top_button").removeClass("active");
         });
-        $('.slick').slick({
-          arrows: false,
-          dots: true,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 3,
-            responsive: [
-                {
-                  breakpoint: 992,
-                  settings: {
+        $(".top_button").click(function () {
+            $("html, body").stop().animate({
+                scrollTop: 0
+            }, "slow", "swing");
+        });
+
+    });
+
+
+    $(".toggle-mnu").click(function () {
+        $(this).toggleClass("on");
+        $(".top_menu ul").slideToggle();
+        return false;
+    });
+
+
+
+
+
+
+    $('.togglemenu').click(function () {
+        $(this).toggleClass('active');
+        $('.top_menu ul').slideToggle(400);
+    });
+    $('.slick').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
                     slidesToShow: 2,
                     slidesToScroll: 1,
-                  }
+                }
                 },
-                {
-                  breakpoint: 768,
-                  settings: {
+            {
+                breakpoint: 768,
+                settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                  }
+                }
                 }
 		// You can unslick at a given breakpoint now by adding:
 		// settings: "unslick"
 		// instead of a settings object
 	  ]
-        });
-    	$(window).scroll(function(){
-        
-            if(window.innerWidth > 768){
-                let top = $('.top-menu').offset().top;
-                $(window).scroll(function(){
-                    if( $(window).scrollTop() > top) {
-                        $('.top-menu').addClass('toTop');
-                    }else{
-                        $('.top-menu').removeClass('toTop');
-                    }
-                });
-            }
-	});
-    
-    
-    
-    
-    
-    
-    
+    });
+    $(window).scroll(function () {
+
+        if (window.innerWidth > 768) {
+            let top = $('.top-menu').offset().top;
+            $(window).scroll(function () {
+                if ($(window).scrollTop() > top) {
+                    $('.top-menu').addClass('toTop');
+                } else {
+                    $('.top-menu').removeClass('toTop');
+                }
+            });
+        }
+    });
+
+
+
+
+
+
+
     $('.mymodal').hide().removeClass("hide");
-	$('#order').click(function(e){
-		e.preventDefault();
-		$('.mymodal').fadeIn(200);
-	});
-	$('.mymodal, .times').click(function(e){
-		e.preventDefault();
-		if ($(e.target).hasClass('mymodal') || $(e.target).hasClass('times')){
-				$('.mymodal').fadeOut(200);
-		}
-		e.stopPropagation();
-	});
-    
-    
-    
-    
-	$('a[href^="#"]').click(function(){
-		let target = $(this).attr("href");
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 800);
-	});
-    
-    
-    
-    
-    
-    
+    $('#order').click(function (e) {
+        e.preventDefault();
+        $('.mymodal').fadeIn(200);
+    });
+    $('.mymodal, .times').click(function (e) {
+        e.preventDefault();
+        if ($(e.target).hasClass('mymodal') || $(e.target).hasClass('times')) {
+            $('.mymodal').fadeOut(200);
+        }
+        e.stopPropagation();
+    });
+
+
+
+
+    $('a[href^="#"]').click(function () {
+        let target = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+    });
+
+
+
+
+
+
 })

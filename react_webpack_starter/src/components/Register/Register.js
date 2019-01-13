e7127ac7ba39d1e3ab4022ba3277e43b9c3d44a5
Vitalii Vitaminenko <vitamin@ukr.net>
2019-01-13 22:27:10 +0200

    Add react starter

--- react_webpack_starter/src/components/Register/Register.js
+++ react_webpack_starter/src/components/Register/Register.js
@@ -14,30 +14,49 @@ class Register extends React.Component {
                 </div>
                 <form action="" className="register__form">
                     <div className="row">
-                        <div className="col-md-4"><label htmlFor="name" className="register__form_label">Name</label><input type="text" name="name" placeholder="Your name" required /></div>
-                        <div className="col-md-4"><label htmlFor="name" className="register__form_label">Email</label><input type="email" name="email" placeholder="Your email" required /></div>
-                        <div className="col-md-4"><label htmlFor="name" className="register__form_label">Phone</label><input type="phone" name="phone" placeholder="+380(__) ___ __ __" required /></div>
-                        <div className="col-md-6">
-                            <select name="register__form_select">
-                                <option value="" >Select your position</option>
-                                <option value="Frontend developer">Frontend developer</option>
-                                <option value="Backend developer">Backend developer</option>
-                                <option value="Lead designer">Lead designer</option>
-                                <option value="QA">QA</option>
-                                <option value="The contextual advertising specialist">The contextual advertising specialist</option>
-                                <option value="Leading specialist of the Control Department">Leading specialist of the Control Department</option>
-                            </select>
+                        <div className="col-md-4">
+                            <div className="register__form_input">
+                                <label htmlFor="name" className="register__form_label">Name</label>
+                                <input type="text" name="name" placeholder="Your name" required />
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="register__form_input">
+                                <label htmlFor="name" className="register__form_label">Email</label>
+                                <input type="email" name="email" placeholder="Your email" required />
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="register__form_input">
+                                <label htmlFor="name" className="register__form_label">Phone</label>
+                                <input type="phone" name="phone" placeholder="+380(__) ___ __ __" required />
+                            </div>
                         </div>
                         <div className="col-md-6">
-                            <select name="register__form_select">
-                                <option value="" >Select your position</option>
-                                <option value="Frontend developer">Frontend developer</option>
-                                <option value="Backend developer">Backend developer</option>
-                                <option value="Lead designer">Lead designer</option>
-                                <option value="QA">QA</option>
-                                <option value="The contextual advertising specialist">The contextual advertising specialist</option>
-                                <option value="Leading specialist of the Control Department">Leading specialist of the Control Department</option>
-                            </select>
+                            <div className="register__form_select">
+                                <select name="register__form_select">
+                                    <option value="">Select your position</option>
+                                    <option value="Frontend developer">Frontend developer</option>
+                                    <option value="Backend developer">Backend developer</option>
+                                    <option value="Lead designer">Lead designer</option>
+                                    <option value="QA">QA</option>
+                                    <option value="The contextual advertising specialist">The contextual advertising specialist</option>
+                                    <option value="Leading specialist of the Control Department">Leading specialist of the Control Department</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="register__form_select">
+                                <select>
+                                    <option value="" >Select your position</option>
+                                    <option value="Frontend developer">Frontend developer</option>
+                                    <option value="Backend developer">Backend developer</option>
+                                    <option value="Lead designer">Lead designer</option>
+                                    <option value="QA">QA</option>
+                                    <option value="The contextual advertising specialist">The contextual advertising specialist</option>
+                                    <option value="Leading specialist of the Control Department">Leading specialist of the Control Department</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
                 </form>
